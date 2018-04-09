07d2c25cb0ec918be58fa193c7f04802fa1f6dca
audrey roumieux <audrey.roumieux@laposte.net>
2018-04-09 09:51:24 +0200

    vendredi soir

--- src/PriceHousePredictionV1.py
+++ src/PriceHousePredictionV1.py
@@ -12,14 +12,14 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import seaborn as sns
 
+from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.model_selection import train_test_split
 
 from sklearn import linear_model
 from sklearn import tree
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.neighbors import KNeighborsRegressor
 
 #%%
 ##### Téléchargement des données
@@ -29,10 +29,10 @@ sample_submission = pd.read_csv('C:\\Users\\b-auroum\\Desktop\\Work\\PROJET\\hou
 
 #%%
 ##### Affichage des données
-
+"""
 print(train.describe())
-# on observe des données manquente parmis les donné numerique. les colonnes sont LotFrontage, MasVnrArea 
-# MasVnrArea, WoodDeckSF, OpenPorchSF, EnclosedPorch, 3SsnPorch, ScreenPorch, PoolArea, MiscVal, MoSold :ont des 0 en median
+# on observe des données manquente parmis les donné numerique. 
+# Des colonnes ont des 0 en median alors qu'elles ont des valeurs moyennes.
  
 print(train.info())
 # données manquentes: LotFrontage, Alley, BsmtExposure, BsmtFinType1, BsmtFinType2, FireplaceQu, GarageYrBlt, GarageFinish, GarageQual, GarageCond, PoolQC, Fence, MiscFeature
@@ -40,10 +40,8 @@ print(train.info())
 print(train.head())
 print(type(train))
 
-print(train.isna().sum())
-
-
 #%%
+
 plt.figure()
 ax = sns.barplot('MSSubClass', 'SalePrice', data = train) 
 ax.set(xlabel = 'building Class', ylabel = 'Sale Price')
@@ -70,59 +68,92 @@ ax.set(xlabel='number of Bedroom', ylabel='Count')
 plt.show()
 
 
-
 plt.figure()
 ax = train.GarageType.hist(bins=15, color='teal', alpha=0.8)
 ax.set(xlabel='Type de Garage', ylabel='Count')
 plt.show()
-
+"""
 #%%
 ##### Prétraitement des données et Création de nouvelle variable
+def type_category(df):
+    binaire = pd.DataFrame()
+    numerique = pd.DataFrame()
+    nominal = pd.DataFrame()
     
+    for column in df :
+        if len(df[column].value_counts()) == 2 :
+            print("var binnaire")
+            encoder = LabelEncoder()
+            try :
+                df[column] = encoder.fit_transform(df[column].astype(str))
+                binaire.assign(column = df[column])
+                print()
+            except :
+                print('Error binnary encoding '+ encoder)
+               
+        elif (df[column].dtype == "float") or (df[column].dtype == "int64"):
+            print("var numerique")
+            numerique.assign(column = df[column])
+           
+        else : #dtype == 'category','object'
+            print("var ordinal/categorielle")
+            nominal.assign(column = df[column])
+            
+    typeCategory = [ binaire, numerique, nominal]           
+    return typeCategory
+
+
+def replace_one_hot_encoding(df):
+    columnsToEncode = df.select_dtypes(include=['category','object'])
+    # df.dtypes[train.dtypes=="object"].index.values
+    
+    for column in columnsToEncode: 
+        #s'il y a trops de category differente dans column, alors on ne traitera pas ces données exp: name_colonne
+        if len(df[column].value_counts()) > 10 : 
+            df = df.drop([column], axis=1)
+        # sinon on fait du one hot encoding dessus
+        else :
+            # Get one hot encoding on columns
+            one_hot = pd.get_dummies(df[column])
+            # Drop columns has it is now encoding
+            df = df.drop([column], axis=1)
+            # Join the encode to df
+            df = df.join(one_hot)
+            
+    return df
+
 def haveGarage(df):
     df['haveGarage'] = df['GarageType'].isna()
     return df
+
 
 def havePool(df):
     df['havePool'] = df['PoolArea'].isna()
     return df
 
-def replace(df):
-    
-    for col in df.dtypes[df.dtypes=="object"].index.values:
-        df[col]
-    
-    #df['GarageType'].replace(to_replace=dict(2Types=6, Attchd=5, Basment=4, BuiltIn=3, CarPort=2, Detchd=1), inplace=True)
-    df['MSZoning'].replace(to_replace = dict(A=7, C=6, FV=5, I=4, RH=3, RL=2, RP=1, RM=0), inplace=True)
-    df['Street'].replace(to_replace = dict(Pave=0, Grvl=1), inplace=True)
-    df['LotShape'].replace(to_replace = dict(Reg=3, IR1=2, IR2=1, IR3=0), inplace=True)
-    df['LandContour'].replace(to_replace = dict(Lvl=3, Bnk=2, HLS=1, Low=0), inplace=True)
-    #df['BldgType'].replace(to_replace = dict(1Fam= 1, 2FmCon = 2, Duplx = 3, TwnhsE = 4, TwnhsI = 5), inplace = True)
-    df.replace(to_replace = dict(Ex=5, Gd=4, TA=3, Fa=2, Po=1), inplace=True)
-    return df
-
 def drop_features(df):
+    na_columns=train.columns[data_train.isna().any()]
+    print(data_train[na_columns].isna().sum())
     return df.drop(['Id'], axis=1)
 
 
 def transform_features(df):
     df = haveGarage(df)
     df = havePool(df)
-    df = replace(df)
-    df = drop_features(df)
+    #df = replace_one_hot_encoding(df)
+    #df = drop_features(df)
     return df
 
 
 data_train = transform_features(train)
 data_test = transform_features(test)
-data_train.head()
+#data_train.head()
 
-for col in ['Alley', 'BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2', 'FireplaceQu','GarageType', 'GarageFinish', 'GarageQual', 'GarageCond', 'PoolQC', 'Fence', 'MiscFeature']:
-    train[col] = data_train[col].astype('category')
-    train[col].cat.codes
-    
+typeCategory = type_category(train)
+
 #%%
 #### Renomage des colonnes
+'''
 data_train.rename(index=str, 
              columns={'MSSubClass': 'BuildingClass', 'MSZoning': 'ZoningClass',
                       'LotArea': 'Aire_m²', 'RoofMatl': 'RoofMaterial', 
@@ -130,50 +161,84 @@ data_train.rename(index=str,
                       'PoolQC':'PiscineQualite', 'Fence':'BarriereQualite',
                       'MiscFeature': 'Divers', 'MiscVal':'ValueDivers', 
                       'MoSold': 'MoisVendu', 'YrSold':'AnneeVendu'})
-print(train.columns)
-
-y = data_train.SalePrice
+#print(train.columns)
+'''
 #%%
 ##### feature importances
 y = data_train.SalePrice
-X = data_train[['MSSubClass', 'LotArea', 'Street', 'LotShape', 'LandContour', 'Utilities', 'LotConfig', 'LandSlope', 'Neighborhood', 'OverallQual', 'OverallCond', 'YearBuilt', 'PoolArea', 'havePool', 'haveGarage']]
+X = data_train.drop(['Id', 'SalePrice', 'LotFrontage', 'MasVnrArea', 'GarageYrBlt'], axis=1)
+Xtest = data_test.drop(['Id', 'LotFrontage', 'MasVnrArea', 'GarageYrBlt'], axis=1)
 
-
+#%%
 # Build a forest and compute the feature importances
 forest = ExtraTreesClassifier(n_estimators = 250, random_state = 0)
-#%%
 forest.fit(X, y)
 
-#%%
 importances = forest.feature_importances_
-std = np.std([tree.feature_importances_ for tree in forest.estimators_], axis=0)
+std = np.std([arbre.feature_importances_ for arbre in forest.estimators_], axis=0)
 indices = np.argsort(importances)[::-1]
 
 # Print the feature ranking
 print("Feature ranking:")
 
+X_select_id_col_feature_importance = []
 for f in range(X.shape[1]):
-    print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
-    
+    if importances[indices[f]] >= 0.02 :
+        print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
+        X_select_id_col_feature_importance.append(indices[f])
+        
+print() 
+
 #%%
+X_col_feature_importance = []
+Xtest_col_feature_importance = []
+for id in X_select_id_col_feature_importance:
+    X_col_feature_importance.append(X.columns[id])
+    Xtest_col_feature_importance.append(X.columns[id])
+    
+X_test_less_col = Xtest[Xtest_col_feature_importance]
+
+
 ##### Split des données Train/Test
-
-
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=.3, random_state=42)
-print(x_train.head())
-print(x_train.describe())
+x_train, x_test, y_train, y_test = train_test_split(X[X_col_feature_importance], y, test_size=.3, random_state=42)
+# print(x_train.head())
+# print(x_train.describe())
 
 #%%
 ##### Entrainement (descente de gradien, affichage d'erreur)
 def entrainement():
     listEnt=[]
     listEnt.append(linear_model.LinearRegression().fit(x_train, y_train))
-    listEnt.append(tree.DecisionTree...().fit(x_train, y_train))
-    listEnt.append(KNeighbors...().fit(x_train, y_train))
-    
+    listEnt.append(tree.DecisionTreeRegressor().fit(x_train, y_train))
+    listEnt.append(KNeighborsRegressor(n_neighbors=5).fit(x_train, y_train))
+    listEnt.append(RandomForestRegressor(max_depth=2, random_state=0).fit(x_train, y_train))
     return listEnt
 
-#entr = entrainement()
+entr = entrainement()
+print(entr)
 
 #%%
 ##### Verification de la performance sur le jeu de donnée test
+print("Train : ")
+for i in entr:
+    print(i.score(x_train, y_train))
+    
+print("Test : ")
+for i in entr:
+    print(i.score(x_test, y_test))
+
+#%%  
+#### Récuperation des données a mettre sur kaagle
+na_columns_test = X_test_less_col.columns[X_test_less_col.isna().any()]
+print(X_test_less_col[na_columns_test].isna().sum())
+
+regKNN = KNeighborsRegressor(n_neighbors=5)
+regKNN.fit(x_train, y_train)
+print(x_train.columns)
+print(Xtest.columns)
+predictTest = regKNN.predict(Xtest)
+
+#%%
+my_submission = pd.DataFrame({'Id': Xtest.Id, 'SalePrice': predictTest}) 
+#my_submission.to_csv('submissionT.csv', index = False)
+my_submission
